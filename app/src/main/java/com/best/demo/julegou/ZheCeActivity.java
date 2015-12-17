7c0e6485d999744f2492d1f3a1ea2c9bbb2541b9
binge123 <912349941@qq.com>
2015-12-17 13:42:01 +0800

    add longin zhuce

--- app/src/main/java/com/best/demo/julegou/ZheCeActivity.java
+++ app/src/main/java/com/best/demo/julegou/ZheCeActivity.java
@@ -4,12 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 
 /**
- * Created by Administrator on 2015/12/16.
+ * Created by Administrator on 2015/12/17.
  */
-public class LoginActivity extends Activity{
+public class ZheCeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_zhuce);
     }
 }
