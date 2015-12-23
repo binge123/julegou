0f483f500d8465f307952765a2ede91b1f35c004
binge123 <912349941@qq.com>
2015-12-23 21:11:33 +0800

    no message

--- app/src/main/java/com/best/fragment/SearchFragment.java
+++ app/src/main/java/com/best/fragment/SearchFragment.java
@@ -10,6 +10,9 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 import com.best.adapter.SearchGridViewAdapter;
@@ -28,6 +31,13 @@ public class SearchFragment extends Fragment {
     GridView gv;
     private MapView mapView;
     private AMap aMap;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明定位回调监听器
+    public AMapLocationListener mLocationListener;
+    //声明mLocationOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
 
     @Nullable
     @Override
@@ -42,7 +52,28 @@ public class SearchFragment extends Fragment {
         mapView = (MapView) v.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);// 必须要写
         aMap = mapView.getMap();
-
+        //初始化定位
+        mLocationClient = new AMapLocationClient(getActivity());
+        //设置定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+        //初始化定位参数
+        mLocationOption = new AMapLocationClientOption();
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置是否返回地址信息（默认返回地址信息）
+        mLocationOption.setNeedAddress(true);
+        //设置是否只定位一次,默认为false
+        mLocationOption.setOnceLocation(false);
+        //设置是否强制刷新WIFI，默认为强制刷新
+        mLocationOption.setWifiActiveScan(true);
+        //设置是否允许模拟位置,默认为false，不允许模拟位置
+        mLocationOption.setMockEnable(false);
+        //设置定位间隔,单位毫秒,默认为2000ms
+        mLocationOption.setInterval(2000);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
 
         final Intent intent = new Intent(getActivity(), ShopDetailsActivity.class);
         gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
